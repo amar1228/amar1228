acc7b8e5ff6fabdaf19cba4396011082c1379571
amar1228 <53291668+amar1228@users.noreply.github.com>
2022-06-24 11:17:01 +0530

    Create FibonacciSeries.java

---
+++ Practical/FibonacciSeries.java
@@ -0,0 +1,20 @@
+package AMAR;
+
+public class FibonaciSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1 = 0 ,n2 = 1,n3 = 0;
+		System.out.print(n1 + " " + n2);    
+		for(int i  = 0 ; i <= 10 ; i++ ) {
+			n3=n1+n2;    
+			System.out.print(" "+n3);    
+			n1=n2;    
+			n2=n3;    
+			
+		}
+		
+
+	}
+
+}
