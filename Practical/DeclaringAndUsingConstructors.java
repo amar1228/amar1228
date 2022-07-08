580bf93f6f35017bd9e9b5a19975839ef090896f
amar1228 <53291668+amar1228@users.noreply.github.com>
2022-07-08 10:32:51 +0530

    Create DeclaringAndUsingConstructors.java

---
+++ Practical/DeclaringAndUsingConstructors.java
@@ -0,0 +1,33 @@
+/*
+ * author : amar gaonkar
+ */
+
+package AMAR;
+
+public class Circle {
+	
+	float radius;
+
+	
+	
+	public Circle() {
+		
+		radius = 1.5f;
+		System.out.println("this is the new radius given = "+ radius);
+	}
+
+}
+
+
+class Shape {
+
+	public static void main(String[] args) {
+		
+		Circle c = new Circle();
+		
+		
+		
+
+	}
+
+}
